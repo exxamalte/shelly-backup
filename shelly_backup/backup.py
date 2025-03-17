8e013fa0801bd37c806dc29ea265dd266fe074fa
Malte Franken <exxamalte@users.noreply.github.com>
2025-03-17 12:32:29 +1100

    allow specifying target folder

--- shelly_backup/backup.py
+++ shelly_backup/backup.py
@@ -1,6 +1,8 @@
 """Shelly Backup."""
+
 import json
 import logging
+from pathlib import Path
 
 import httpx
 
@@ -10,21 +12,30 @@ _LOGGER = logging.getLogger(__name__)
 class Backup:
     """Shelly Backup."""
 
-    def __init__(self, ip_address: str):
+    _ip_address: str | None = None
+    _target_folder: Path | None = None
+
+    def __init__(self, ip_address: str, target_folder: str):
         """Initialise backup."""
         self._ip_address = ip_address
+        if target_folder:
+            # Create folder structure if it doesn't exist.
+            Path(target_folder).mkdir(parents=True, exist_ok=True)
+            self._target_folder = Path(target_folder)
+        else:
+            self._target_folder = Path.cwd()
 
     def execute(self):
         """Run device backup."""
         try:
             # Get some basic information from the device to determine which generation
-            url: str = f'http://{self._ip_address}/shelly'
+            url: str = f"http://{self._ip_address}/shelly"
             response = httpx.get(url)
             if response.status_code == httpx.codes.OK:
                 response_json = response.json()
                 _LOGGER.info(response_json)
                 # Check for 'gen' tag 2 (with 1 being the default)
-                shelly_gen = response_json.get('gen', 1)
+                shelly_gen = response_json.get("gen", 1)
                 _LOGGER.info("Device identified as generation %s", shelly_gen)
                 match shelly_gen:
                     case 1:
@@ -35,25 +46,29 @@ class Backup:
                         # Unknown generation.
                         _LOGGER.warning("Unknown generation: %s", shelly_gen)
             else:
-                _LOGGER.warning("Unable to get basic device information from %s, response code %s",
-                                url,
-                                response.status_code)
+                _LOGGER.warning(
+                    "Unable to get basic device information from %s, response code %s",
+                    url,
+                    response.status_code,
+                )
         except httpx.HTTPError as exc:
-            _LOGGER.warning(f"HTTP Exception for %s - %s", exc.request.url, exc)
+            _LOGGER.warning("HTTP Exception for %s - %s", exc.request.url, exc)
 
     def _generation_1(self, basic_device_information: dict):
         """Backup generation 1 device."""
-        urls: dict = {'settings': f'http://{self._ip_address}/settings',
-                      'actions': f'http://{self._ip_address}/settings/actions',
-                      'status': f'http://{self._ip_address}/status'}
+        urls: dict = {
+            "settings": f"http://{self._ip_address}/settings",
+            "actions": f"http://{self._ip_address}/settings/actions",
+            "status": f"http://{self._ip_address}/status",
+        }
         self._configuration(urls)
 
     def _generation_2(self, basic_device_information: dict):
         """Backup generation 2 device."""
         urls: dict = {
-            "configuration": f'http://{self._ip_address}/rpc/Shelly.GetConfig',
-            "device_info": f'http://{self._ip_address}/rpc/Shelly.GetDeviceInfo',
-            "status": f'http://{self._ip_address}/rpc/Shelly.GetStatus',
+            "configuration": f"http://{self._ip_address}/rpc/Shelly.GetConfig",
+            "device_info": f"http://{self._ip_address}/rpc/Shelly.GetDeviceInfo",
+            "status": f"http://{self._ip_address}/rpc/Shelly.GetStatus",
         }
         self._configuration(urls)
 
@@ -65,12 +80,16 @@ class Backup:
                 response_json = response.json()
                 self._store(information_type, response_json)
             else:
-                _LOGGER.warning("Unable to get device settings from %s, response code %s",
-                                url,
-                                response.status_code)
+                _LOGGER.warning(
+                    "Unable to get device settings from %s, response code %s",
+                    url,
+                    response.status_code,
+                )
 
     def _store(self, information_type: str, device_settings: dict):
         """Store device settings."""
         filename: str = f"shelly-{self._ip_address}-{information_type}.json"
-        with open(filename, 'w') as file:
+        filepath = self._target_folder.joinpath(filename)
+        with open(filepath, "w") as file:
             json.dump(device_settings, file, indent=2)
+            _LOGGER.info("Configuration stored: %s", filepath)
--- shelly_backup/cli.py
+++ shelly_backup/cli.py
@@ -1,10 +1,11 @@
 """Shelly Backup CLI tool."""
+
 import argparse
 import importlib.metadata
 import logging
 
-from .consts import APPLICATION_NAME
 from .backup import Backup
+from .consts import APPLICATION_NAME
 
 
 def main():
@@ -13,15 +14,32 @@ def main():
         level=logging.INFO,
     )
     logger = logging.getLogger(__name__)
-    logger.info("%s started (version %s)...",
-                APPLICATION_NAME, importlib.metadata.version("shelly_backup"))
+    logger.info(
+        "%s started (version %s)...",
+        APPLICATION_NAME,
+        importlib.metadata.version("shelly_backup"),
+    )
 
-    parser = argparse.ArgumentParser(description='Shelly Backup')
-    parser.add_argument('--ip-address', dest='ip_address', required=True, help='IP address of the Shelly device')
+    parser = argparse.ArgumentParser(description="Shelly Backup")
+    parser.add_argument(
+        "--ip-address",
+        dest="ip_address",
+        required=True,
+        help="IP address of the Shelly device",
+    )
+    parser.add_argument(
+        "--target",
+        dest="target",
+        required=False,
+        help="Target folder where configuration will be stored",
+    )
     args = parser.parse_args()
-    # Execute backup
-    backup = Backup(args.ip_address)
-    backup.execute()
+    try:
+        # Execute backup
+        backup = Backup(args.ip_address, args.target)
+        backup.execute()
+    except OSError as e:
+        logger.error("Unable to create device backup: %s", e)
 
 
 if __name__ == "__main__":
